67616caf6d925c749fc45f941e7b821e84fe23a9
Cody Hillyard <codyhillyard@gmail.com>
2019-05-17 20:24:34 -0600

    working on nav

--- src/comps/header/header.js
+++ src/comps/header/header.js
@@ -1,28 +1,36 @@
 import React, { Component } from "react"; 
 
 export default class Header extends Component {
+  constructor(props){
+    super(props); 
+    this.state = {
+      selected : "home"
+    }
+  }
+
+  
+
   render() {
     return (
       <div className="ch-header">
-        <nav className="navbar navbar-inverse navbar-fixed-top">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>                        
-              </button>
-              <a className="navbar-brand" href="#image_header">Cody Hillyard</a>
-            </div>
-            <div className="collapse navbar-collapse" id="myNavbar">
-              <ul className="nav navbar-nav">
-                <li><a href="#home">Home</a></li>			
-                <li><a href="#resume">Resume</a></li>
-                <li><a href="#blog">Blog</a></li>
-                <li><a href="#projects">Projects</a></li>
-              </ul>
-            </div></div>
-        </nav>
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/">Home</a> 
+          </li>
+         
+          <li className="nav-item">
+            <a className="nav-link" href="/">History</a> 
+          </li>
+         
+          <li className="nav-item">
+            <a className="nav-link" href="/">Blog</a> 
+          </li>
+         
+          <li className="nav-item">
+            <a className="nav-link" href="/">Projects</a> 
+          </li>
+         
+        </ul>
       </div>
     );
   }
--- src/comps/site.js
+++ src/comps/site.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Header from "./header/header"; 
 
 export default class Site extends Component {
   render() {
     return (
       <div>
+        <Header />
         Cody do work here
       </div>
     );
